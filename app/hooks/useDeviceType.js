af1db2816a586011230ba9f25162971179fe365f
Agwam Ayatullah <agwamayatullah@gmail.com>
2024-07-01 22:36:50 +0800

    add md:design to the footer

--- app/hooks/useDeviceType.js
+++ app/hooks/useDeviceType.js
@@ -2,9 +2,7 @@ import { useMediaQuery } from '@uidotdev/usehooks';
 
 const useDeviceType = () => {
   const isSmallDevice = useMediaQuery('only screen and (max-width: 640px)');
-  const isMediumDevice = useMediaQuery(
-    'only screen and (min-width: 769px) and (max-width: 1024px)'
-  );
+  const isMediumDevice = useMediaQuery('only screen and (max-width: 1024px)');
   const isLargeDevice = useMediaQuery('only screen and (min-width: 1024px)');
   const isExtraLargeDevice = useMediaQuery(
     'only screen and (min-width: 1201px)'
