849d037045a939a81127e8183a590c29ace73093
agwam <agwamayatullah@gmail.com>
2024-07-10 14:51:01 +0800

    add mobile design to carepin

--- app/_components/CarePin.jsx
+++ app/_components/CarePin.jsx
@@ -135,32 +135,34 @@ function CarePin() {
     <section ref={container} id='care-section'>
       <div className='care-container flex flex-col items-center'>
         <div className='h-[100vh] sticky top-0'>
-          <div className='flex flex-col card-container care-card1 px-20 pt-12 py-20 bg-black-100 justify-center items-start rounded-2xl gap-8 relative top-[15%] min-w-fit'>
+          <div className='flex flex-col card-container care-card1 px-8 py-6 sm:px-20 sm:pt-12 sm:py-20 bg-black-100 justify-center items-start rounded-2xl gap-8 relative top-[15%] min-w-fit overflow-hidden'>
             <img
               src='./illustration-percentage.svg'
               alt='percentage illustration'
-              className='relative -left-24'
+              className='relative -left-[3rem] sm:-left-24 max-sm:w-[5rem]'
             />
 
-            <div className='flex items-start gap-4 text-white'>
-              <div className='w-[10rem] h-[6rem] text-center'>
+            <div className='flex items-start gap-4 text-white max-md:flex-col'>
+              <div className='w-fit sm:w-[10rem] sm:h-[6rem] text-center'>
                 <FlipWords
-                  className={'font-d3 text-center text-white'}
+                  className={
+                    'font-d3 text-center text-white max-sm:text-[3rem] max-sm:px-0'
+                  }
                   words={infoList.map((item) => item.number)}
                   duration={4000}
                 />
               </div>
 
-              <div className='w-[20rem] text-wrap flex flex-col gap-2'>
+              <div className=' w-[16rem] sm:w-[20rem] text-wrap flex flex-col gap-2'>
                 <FlipSentence
                   words={infoList.map((item) => item.text)}
-                  className={'font-h5 text-white'}
+                  className={'font-h5 text-white max-sm:text-[1.2rem]'}
                   duration={4000}
                 />
                 <FlipWords
                   words={infoList.map((item) => item.source)}
                   duration={4000}
-                  className={'text text-gray-300 px-0'}
+                  className={'text-gray-300 px-0 max-sm:text-[0.875rem]'}
                 />
               </div>
             </div>
@@ -168,15 +170,15 @@ function CarePin() {
         </div>
 
         <div className='sticky top-0 h-[100vh]'>
-          <div className='care-card2 flex flex-col card-container px-20 py-20 bg-black-100 justify-center items-center rounded-2xl gap-8 z-[2] relative top-[20%] md:w-[40rem] mb-[100vh]'>
+          <div className='care-card2 flex flex-col card-container px-8 py-8 sm:px-20 sm:py-20 bg-black-100 justify-center items-center rounded-2xl gap-8 z-[2] relative top-[20%] md:w-[40rem] mb-[100vh]'>
             <div className='relative' id='cursor-card'>
-              <h5 className='text-center text-white border border-dashed px-20 py-20 relative'>
+              <h2 className='font-h5 text-center text-white border border-dashed py-20 px-16 sm:px-20 sm:py-20 relative max-sm:text-[1.2rem]'>
                 <span>
                   Seeking someone <br />
                   who can steer clear <br />
                 </span>
                 <span className='text-green mt-[50vh]'>this problem?</span>
-              </h5>
+              </h2>
 
               <div className='w-4 h-4 bg-white absolute -top-2 -left-2'></div>
               <div className='w-4 h-4 bg-white absolute -bottom-2 -left-2'></div>
@@ -188,7 +190,7 @@ function CarePin() {
 
         <div className='relative top-0 min-h-[100vh] care-card3 pb-96 bg-black-100'>
           <div className='card-container px-20 py-20 rounded-2xl z-[3] relative w-[100vw] min-h-[100vh] flex flex-col items-center justify-center'>
-            <h1 className='text-center text-white leading-[125%] md:text-[2.5rem] lg:text-[3.5rem]'>
+            <h2 className='text-center text-white leading-[125%] text-[1.25rem] sm:text-[2.5rem] lg:text-[3.5rem] font-normal'>
               <span className='absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 hidden-word'>
                 Dont worry, <br /> you just found{' '}
                 <span className='anim-green2'>one</span>.
@@ -203,9 +205,9 @@ function CarePin() {
               <span className='opacity-0 absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 unhidden-word2'>
                 here are the 3 steps we take to make it happen.
               </span>
-            </h1>
+            </h2>
 
-            <div className='grid md:max-lg:grid-cols-2 lg:flex lg:flex-row gap-12 md:max-lg:gap-40 w-fit z-10'>
+            <div className='flex max-sm:flex-col max-sm:absolute max-sm:top-32 sm:grid md:max-lg:grid-cols-2 lg:flex lg:flex-row gap-12 md:max-lg:gap-40 w-fit z-10'>
               {careList.map(
                 ({ title, isDown, text, bgColor, bgImg, handImg, gif }, i) => {
                   return (
@@ -265,7 +267,7 @@ function Card({
 function NewCard({ text, title, gif, id }) {
   return (
     <div
-      className={`flex flex-col gap-6 w-[17rem] h-[22rem] lg:w-[19.5rem] lg:h-[25rem] py-12 px-6 rounded-[2.5rem] bg-[#fff] justify-end relative care-main-card overflow-hidden ${
+      className={`shadow-[-3.538px_3.538px_22.115px_0px_rgba(255,255,255,0.3)] flex flex-col gap-6 w-[17rem] h-[22rem] lg:w-[19.5rem] lg:h-[25rem] py-12 px-6 rounded-[2.5rem] bg-[#fff] justify-end relative care-main-card overflow-hidden ${
         id === 2 && 'md:max-lg:row-span-2 md:max-lg:my-auto'
       }`}
     >
