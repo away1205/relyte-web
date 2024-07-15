67b73a4a8dbc647d99052b853439424b6f3a0281
agwam <agwamayatullah@gmail.com>
2024-07-16 03:44:02 +0800

    change the content in faq

--- app/_components/FaqSection.jsx
+++ app/_components/FaqSection.jsx
@@ -4,19 +4,19 @@ const faqList = [
   {
     question: "What are the ongoing cost?",
     answer:
-      "For just $45 a month, we offer hosting and support packages that include an SSL certificate, a Content Delivery Network, and our ongoing support service, ensuring your site is safeguarded, blazing fast, accessible worldwide, and backed by our friendly team always ready to assist with any technical issues.",
+      "For just $99 a month, we offer hosting and support packages that include an SSL certificate, a Content Delivery Network, and our ongoing support service, ensuring your site is safeguarded, blazing fast, accessible worldwide, and backed by our friendly team always ready to assist with any technical issues.",
   },
   {
     question: "How much do you charge for a website design and build?",
     oldAnswer:
       "Our pricing varies based on the unique design and structure of your site, so we can't give an exact quote until we learn more about your project. However, we specialize in creating stunning, high-end, modern websites that are custom-designed just for you. Each design and line of code is crafted in-house with love, avoiding any pre-made themes or clunky templates. Once we understand the scale and scope of your vision, we can provide a detailed pricing guide. We're excited to bring your dream website to life!",
     answer:
-      "Our pricing is tailored to the unique design and structure of your site, so we need to learn more about your project to provide an exact quote. But here’s the exciting part: for our very first client and for a limited time, we're offering our stunning website design and development services completely free! Yes, the only cost you'll have is for the server. Don't miss this incredible opportunity—let's bring your vision to life today!",
+      "Our pricing is tailored to the unique design and structure of your site, so we need to learn more about your project to provide an exact quote. But here’s the exciting part: for our very first client, we're offering our stunning website design and development services completely free! Yes, the only cost you'll have is for the server. Don't miss this incredible opportunity—let's bring your vision to life today!",
   },
   {
     question: "Can I make updates to my custom website after it is completed?",
     answer:
-      "Absolutely! Our custom web designs not only allow you to easily update and modify all content after development, but we also offer immediate support and assistance whenever needed. With our ongoing support and maintenance packages, major website updates and changes are taken care of, while minor tweaks and fixes are included in our web hosting packages. Plus, if you need new custom sections or pages, we can provide quick and accurate estimates. Get started with us now and experience seamless web management!",
+      "Absolutely! Our custom web designs not only allow you to easily update and modify all content after development, but we also offer immediate support and assistance whenever needed. With our ongoing support and maintenance packages, major website updates and changes are taken care of, while minor tweaks and fixes are included in our web hosting packages. Plus, if you need new custom sections or pages, we can provide quick and accurate estimates.",
   },
   {
     question: "Do you create the content for our new website?",
@@ -26,12 +26,12 @@ const faqList = [
   {
     question: "Is SEO included in my web design project?",
     answer:
-      "All our web design projects come with a free SEO guide where we equip you with all the necessary tools to monitor search engine traffic and get the best possible search results.",
+      "Sure, every web design project we take on includes a free SEO guide! We'll give you all the tools you need to track your search engine traffic and achieve the best possible search results. And if you want to take it a step further, why not try our professional SEO services? We're here to help you boost your visibility and drive even more traffic to your site!",
   },
   {
     question: "Do I need a blog with my website?",
     answer:
-      "Having a blog integrated into your web design is a great strategy for extending your website content without cluttering your site. Ideally, you will post interesting articles or tools that other websites can backlink to as a resource.\n\n This content will have popular keyword phrases and be found in search engines, improving your site’s traffic and general rankings. For example, you can post a regular newsfeed, give away useful resources and articles related to your field, show off media publicity or give away industry tips and tricks.\n\n Blogs bring an added dimension to your website by allowing your customers to interact and engage with dynamic content on your website. We can also make it so you can add related blog posts to your existing core pages and content, so it takes the user on a complete journey, ending with a call-to-action.",
+      "Adding a blog to your website is a fantastic way to grow your content without making your site feel cluttered. By sharing fun articles or handy tools, you give other sites a reason to link back to you, which can really boost your traffic and search engine rankings. Think about posting regular updates, offering free resources, sharing industry news, or providing helpful tips and tricks. A blog makes your website more engaging, giving your visitors something to interact with. Plus, you can connect your blog posts to your main content, leading users on a complete journey that ends with a call-to-action.",
   },
 ];
 
