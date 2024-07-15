b95ea14ef2f29bde50d4491fdd95f2e51376e2b5
agwam <agwamayatullah@gmail.com>
2024-07-16 04:17:32 +0800

    create a sitemap in app

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+function sitemap() {
+  return [
+    {
+      url: "https://relyte.space/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://relyte.space/projects",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://relyte.space/blog",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
+
+export default sitemap;
