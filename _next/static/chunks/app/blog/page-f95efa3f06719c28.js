89aa9e89d91aa94daad393774b0812bd2ade9688
away1205 <135943738+away1205@users.noreply.github.com>
2024-08-02 14:52:47 +0000

    Build: (1964bf3) fix the animatedlink make the page disapper in dynamic routes, specifically in blog

--- _next/static/chunks/app/blog/page-f95efa3f06719c28.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[404],{5476:function(e,n,s){Promise.resolve().then(s.bind(s,2460)),Promise.resolve().then(s.bind(s,5667)),Promise.resolve().then(s.t.bind(s,8173,23)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[922,231,78,947,460,971,23,744],function(){return e(e.s=5476)}),_N_E=e.O()}]);
