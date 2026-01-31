5647978941854750f19fc8f34b3fceecc8f87478
Agwam <agwamayatullah@gmail.com>
2026-02-01 01:29:28 +0800

    Update resend api key

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import EmailContactTemplate from "../../_components/EmailContactTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || "TEST_API_KEY");
 
 export async function POST(request) {
   const obj = {
